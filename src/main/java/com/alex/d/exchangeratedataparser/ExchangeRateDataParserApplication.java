ad80afcb5f05514b9f00a576f61240a8a8fde3b7
smlqqq <78303132+smlqqq@users.noreply.github.com>
2024-07-27 20:09:21 +0300

    fix

--- src/main/java/com/alex/d/exchangeratedataparser/ExchangeRateDataParserApplication.java
+++ src/main/java/com/alex/d/exchangeratedataparser/ExchangeRateDataParserApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableCaching
 public class ExchangeRateDataParserApplication {
 
     public static void main(String[] args) {
